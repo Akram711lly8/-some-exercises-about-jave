f9494f5d861815c2d0a56f729c8ef427590e3dd5
Akram711lly8 <112018176+Akram711lly8@users.noreply.github.com>
2022-11-12 20:20:10 +0300

    Add files via upload

---
+++ Registration system traffic violations/src/GUL.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class GUL {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField jhjmTextField;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+}
